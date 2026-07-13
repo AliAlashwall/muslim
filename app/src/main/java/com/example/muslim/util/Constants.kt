771f4c39be74ec9bef5b67b5b5e3d52d311be895
Ali <alialashwall14@gmail.com>
2026-07-13 06:26:45 +0300

    Add prayers name as constants to avoid hard coded writing

--- app/src/main/java/com/example/muslim/util/Constants.kt
+++ app/src/main/java/com/example/muslim/util/Constants.kt
@@ -2,4 +2,9 @@ package com.example.muslim.util
 
 object Constants {
     const val API_KEY = "H7YcdfNpBNsCy7JKMc7tHUMY9lxzVcN6amFYzO8jrONBHJwK"
+    const val FAJR = "Fajr prayer"
+    const val DHUHR = "Dhuhr prayer"
+    const val ASR = "Asr prayer"
+    const val MAGHRIB = "Maghrib prayer"
+    const val ISHA = "Isha prayer"
 }
