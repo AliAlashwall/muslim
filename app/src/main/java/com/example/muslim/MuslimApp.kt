457bd4b8eb6999495adc3a4333a3aae34733105d
Ali <alialashwall14@gmail.com>
2026-07-15 17:33:41 +0300

    setup the NotificationChannel at the start of the application

--- app/src/main/java/com/example/muslim/MuslimApp.kt
+++ app/src/main/java/com/example/muslim/MuslimApp.kt
@@ -1,8 +1,24 @@
 package com.example.muslim
 
 import android.app.Application
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.os.Build
+import androidx.annotation.RequiresApi
 import dagger.hilt.android.HiltAndroidApp
 
 
 @HiltAndroidApp
-class MuslimApp : Application()
+class MuslimApp : Application() {
+    @RequiresApi(Build.VERSION_CODES.O)
+    override fun onCreate() {
+        super.onCreate()
+        val channel = NotificationChannel(
+            "alarm_channel_two", "Alarms", NotificationManager.IMPORTANCE_HIGH
+        ).apply {
+            setSound(null, null)
+            enableVibration(true)
+        }
+        getSystemService(NotificationManager::class.java).createNotificationChannel(channel)
+    }
+}
