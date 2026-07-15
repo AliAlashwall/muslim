8765580d88f6eff099100be1eafb4dcdc31773c4
Ali <alialashwall14@gmail.com>
2026-07-15 17:22:40 +0300

    change the version of the database

--- app/src/main/java/com/example/muslim/data/local/database/AppDatabase.kt
+++ app/src/main/java/com/example/muslim/data/local/database/AppDatabase.kt
@@ -6,7 +6,7 @@ import androidx.room.TypeConverters
 import com.example.muslim.data.local.database.dao.AlarmDao
 import com.example.muslim.data.local.database.entity.AlarmEntity
 
-@Database(entities = [AlarmEntity::class], version = 2, exportSchema = false)
+@Database(entities = [AlarmEntity::class], version = 3, exportSchema = false)
 @TypeConverters(Converters::class)
 abstract class AppDatabase : RoomDatabase() {
 
